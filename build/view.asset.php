23b453fe0cd5400bf1b2b8784386944299d6cc89
JuanMa <juanma.garrido@gmail.com>
2025-03-20 18:59:06 +0000

    Refactor webpack configuration to import both non-module and module default configurations from @wordpress/scripts, allowing for enhanced flexibility in build processes.

---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a7cd3fc0a0f5cf32da2b', 'type' => 'module');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
-// Import the original config from the @wordpress/scripts package.
-const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const [
+	defaultConfigNonModule,
+	defaultConfigModule,
+] = require("@wordpress/scripts/config/webpack.config");
 
-// Add any a new entry point by extending the webpack config.
-module.exports = {
-	...defaultConfig,
-};
+module.exports = [defaultConfigNonModule, defaultConfigModule];
